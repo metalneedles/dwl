81dfc538005a2255afdab75bc5376e6abed01a7f
metalneedles <arturdulzon400+github@gmail.com>
2025-12-24 00:02:07 +0100

    update keybind, disable natural scrolling

--- config.def.h
+++ config.def.h
@@ -77,7 +77,7 @@ static const int repeat_delay = 400;
 static const int tap_to_click = 1;
 static const int tap_and_drag = 1;
 static const int drag_lock = 1;
-static const int natural_scrolling = 1;
+static const int natural_scrolling = 0;
 static const int disable_while_typing = 1;
 static const int left_handed = 0;
 static const int middle_button_emulation = 0;
@@ -108,7 +108,7 @@ LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT
 LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE
 */
 static const enum libinput_config_accel_profile accel_profile = LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT;
-static const double accel_speed = 0.0;
+static const double accel_speed = -1.0;
 
 /* You can choose between:
 LIBINPUT_CONFIG_TAP_MAP_LRM -- 1/2/3 finger tap maps to left/right/middle
@@ -140,7 +140,7 @@ static const char *filemanagercmd[] = { "nautilus", NULL };
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
-	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
+	{ MODKEY,                    XKB_KEY_d,          spawn,          {.v = menucmd} },
 	{ MODKEY,                    XKB_KEY_Return,     spawn,          {.v = termcmd} },
   { MODKEY,                    XKB_KEY_e,          spawn,          {.v = filemanagercmd} },
 	{ MODKEY,                    XKB_KEY_b,          togglebar,      {0} },
